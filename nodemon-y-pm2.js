e3a551c7bbbe281fce45b65d37007e9344bf354f
David Morales <morald01@stepstone.com>
2020-07-23 13:50:26 -0600

    Aprendí nuevas herramientas para node.js Nodemon y PM2

---
+++ nodemon-y-pm2.js
@@ -0,0 +1,16 @@
+//Nodemon es una herramienta que se ocupa en desarrollo
+//Nodemon nos ayuda a ejecutar el archivo y que tenga "hotreload" al detectar cambios
+//Para instalarlo es npm install -g nodemon
+//Para ejecutarlo es nodemon (n) que n es el archivo en el que estamos trabajando
+//Para Windows se debe correr npx nodemon (n)
+
+console.log("test");
+console.log("another test");
+
+//PM2 es una herramienta para usar en producción
+//Es parecido a nodemon pero más avanzada y compleja
+//Para instalarlo es npm install -g pm2
+//Para ejecutarlo es pm2 start (n) que n es el archivo en el que estamos trabajando
+//Para Windows se debe correr npx pm2 start (n)
+//El comando pm2 monitor sirve para monitorear cómo va en producción algo
+//Para saber que tenemos o no tenemos corriendo lo vemos con pm2 status
